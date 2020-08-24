7fd61a624183e3c348eacf387d97d2c7915a48d5
IngaPedane <69342996+IngaPedane@users.noreply.github.com>
2020-08-24 21:07:37 +0300

    Add files via upload

--- Python/D2B_final.py
+++ Python/D2B_final.py
@@ -1,18 +1,22 @@
+class NavPozitivsSkaitlis(UserWarning): 
+	pass
+      
 def binary(n):
    if n > 1:
        binary(n//2)
        print(n % 2,end = '')
+       
 while True: 
-    dec = input("Ievadiet pozitīvu skaitli: ") 
-    try: 
-        dec = int(dec) 
-        if dec <= 0: 
-            raise NavPozitivsSkaitlis 
-        break 
-    except ValueError: 
-        print("Ievadītā vērtība nav skaitlis! Mēģiniet vēlreiz: ") 
-    except NavPozitivsSkaitlis: 
-        print("Ievadītā vērtība nav pozitīvs skaitlis! Mēģiniet vēlreiz: ")
+	dec = input("Ievadiet pozitīvu skaitli: ") 
+	try: 
+		dec = int(dec) 
+		if dec <= 0: 
+			raise NavPozitivsSkaitlis 
+		break 
+	except ValueError: 
+		print("Ievadītā vērtība nav skaitlis! Mēģiniet vēlreiz: ") 
+	except NavPozitivsSkaitlis: 
+		print("Ievadītā vērtība nav pozitīvs skaitlis! Mēģiniet vēlreiz: ")
         
 ### https://www.quora.com/How-can-I-make-sure-the-user-inputs-a-positive-integer-in-Python
 
