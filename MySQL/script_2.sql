3566e8ea62e5505d02df0218d031fb0bb1df2946
user <inga.pedane@gmail.com>
2020-09-03 17:00:32 +0300

    20200903_17_00

---
+++ MySQL/script_2.sql
@@ -0,0 +1,24 @@
+CREATE DEFINER=`u24`@`%` FUNCTION `MyFUNC_7`(solis int) RETURNS int(11)
+BEGIN 
+	DECLARE lielums, done int;
+    DECLARE mainig1 integer;
+	DECLARE mainig2 varchar(10);
+    
+    DECLARE cursors_ieksh_tabulas CURSOR FOR SELECT ID, FName FROM MyTBL_7A;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    
+    SET lielums = 0;
+    SET done = FALSE;
+    OPEN cursors_ieksh_tabulas;
+    lasiishanas_cikls: LOOP
+		FETCH cursors_ieksh_tabulas INTO mainig1, mainig2;
+        IF done THEN
+			LEAVE lasiishanas_cikls;
+		END IF;
+        SET lielums = lielums + mainig1;
+	END LOOP;
+	CLOSE cursors_ieksh_tabulas;
+    
+    RETURN lielums;
+    
+END
